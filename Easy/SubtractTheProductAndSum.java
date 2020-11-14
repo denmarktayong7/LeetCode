d318f0dc98e554fdde080e77130443873aa7e8c4
denmarktayong7 <59756769+denmarktayong7@users.noreply.github.com>
2020-11-15 04:12:06 +0800

    Update SubtractTheProductAndSum.java

--- Easy/SubtractTheProductAndSum.java
+++ Easy/SubtractTheProductAndSum.java
@@ -5,8 +5,8 @@ class Solution {
         
         String nStr = Integer.toString(n);
         for (int i = 0; i < nStr.length(); i++) {
-            product *= Integer.parseInt(nStr.charAt(i) + "");
-            sum += Integer.parseInt(nStr.charAt(i) + "");
+            product *= Integer.parseInt(Character.toString(nStr.charAt(i)));
+            sum += Integer.parseInt(Character.toString(nStr.charAt(i)));
         }
         
         return product - sum;
